5c93c2b8f2a09c602f69242958dbb8caeaaee050
Alphactory <aedan.e.taylor@gmail.com>
2019-10-28 17:49:10 -0400

    waits before pinging so not every reinitialization pings.

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ client = slack.WebClient(token=slack_token)
 
 def main():
 	while True:
+		print("yeet")
+		time.sleep(3600)
 		client.chat_postMessage(
 		channel="#general",
 		text="<@UPQ6G4BFE> Bring me the fucking toga!"
 		)
-		time.sleep(3600)
 
 
 if __name__ == '__main__':
